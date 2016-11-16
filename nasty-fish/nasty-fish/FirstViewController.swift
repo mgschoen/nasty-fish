fada51b4efe1b9590026bc100ff500e27310f5fe
Martin Schoen <schoenm@cip.ifi.lmu.de>
2016-11-16 15:06:06 +0100

    Initial commit

---
+++ nasty-fish/nasty-fish/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nasty-fish
+//
+//  Created by Martin Schön on 16.11.16.
+//  Copyright © 2016 Gruppe 08. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ nasty-fish/nasty-fish/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  nasty-fish
+//
+//  Created by Martin Schön on 16.11.16.
+//  Copyright © 2016 Gruppe 08. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
