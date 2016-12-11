2a66c0c51e5dee5004ade7f2798c747d8ba3d374
Manuel Hartmann <manuel.hartmann@mailbox.org>
2016-12-11 12:44:01 +0100

    implemented issue #7 - Search in Transactions

--- nasty-fish/nasty-fish/MoreController.swift
+++ nasty-fish/nasty-fish/MoreController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class MoreController: UITableViewController {
-
+    
+    @IBAction func initWithDefaultData(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,15 +30,15 @@ class MoreController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- nasty-fish/nasty-fish/TransactionsViewController.swift
+++ nasty-fish/nasty-fish/TransactionsViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 import CoreData
 
-class TransactionsViewController: UITableViewController {
-
-    let dataController = DataController.init()
+class TransactionsViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate {
     
-    var transactions = [NSManagedObject]()
+    // https://www.raywenderlich.com/113772/uisearchcontroller-tutorial
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var transactions = [Transaction]()
+    var filteredTransactions = [Transaction]()
+    
     let descriptions = ["Döner (4,- €)", "Per Anhalter durch die Galaxis", "Socken"]
     let peers = ["Martin", "Qend Ressa", "Michael"]
+    
+    // MARK: - @IBAction
     
     @IBAction func unwindSegue(segue: UIStoryboardSegue) {
 
     }
     
+    // MARK: - viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +37,24 @@ class TransactionsViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        transactions = dataController.fetchTransactions()
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.scopeButtonTitles = ["All", "Loan", "Debt", "Past"]
+        searchController.searchBar.delegate = self
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
+        
+//        searchController.searchResultsUpdater = self
+//        searchController.hidesNavigationBarDuringPresentation = false
+//        searchController.dimsBackgroundDuringPresentation = false
+//        searchController.searchBar.sizeToFit()
+//        self.tableView.tableHeaderView = searchController.searchBar
+        
+        transactions = ((UIApplication.shared.delegate as! AppDelegate).dataController?.fetchTransactions())!
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,27 +70,32 @@ class TransactionsViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-//        return 3
-        return descriptions.count
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredTransactions.count
+        }
+        return transactions.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
+//        cell.textLabel?.text = descriptions[indexPath.item]
+//        cell.detailTextLabel?.text = peers[indexPath.item]
+//        cell.im.imageView = Assets
 //        return cell
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
-        cell.textLabel?.text = descriptions[indexPath.item]
-        cell.detailTextLabel?.text = peers[indexPath.item]
-//        cell.im.imageView = Assets
+        let transaction: Transaction
+        if searchController.isActive && searchController.searchBar.text != "" {
+            transaction = filteredTransactions[indexPath.row]
+        } else {
+            transaction = transactions[indexPath.row]
+        }
+        cell.textLabel?.text = transaction.itemDescription
+        cell.detailTextLabel?.text = transaction.peer?.customName
+        // cell.im.imageView = Assets
         return cell
     }
  
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -112,5 +140,44 @@ class TransactionsViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: - UISearchResultsUpdating
+    @available(iOS 8.0, *)
+    public func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        filterContentForSearchText(searchText: searchController.searchBar.text!, scope: scope)
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchText: searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredTransactions = transactions.filter { transaction in
+            let categoryMatch = (scope == "All") || (transaction.category == scope)
+            return categoryMatch && (transaction.itemDescription?.lowercased().contains(searchText.lowercased()))!
+        }
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: - Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let transaction: Transaction
+                if searchController.isActive && searchController.searchBar.text != "" {
+                    transaction = filteredTransactions[indexPath.row]
+                } else {
+                    transaction = transactions[indexPath.row]
+                }
+//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//                controller.detailCandy = transaction
+//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
 }
