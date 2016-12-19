c08017620e17273a41d02f391f074a8e65b511bc
Qendresa Iljazi <iljazi@cip.ifi.lmu.de>
2016-12-19 13:46:12 +0100

    detailView

---
+++ nasty-fish/nasty-fish/TransactionDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TransactionDetailViewController.swift
+//  nasty-fish
+//
+//  Created by Qendresa Iljazi on 19.12.16.
+//  Copyright © 2016 Gruppe 08. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TransactionDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var transactionDescription: UITextField!
+    
+    @IBOutlet weak var peerPicker: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//if Money 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
