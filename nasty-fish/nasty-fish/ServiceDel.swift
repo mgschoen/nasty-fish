89656b24cffe397f75d57bebea74721cacd39552
Micheal Gambitz <gambitz@cip.ifi.lmu.de>
2017-01-11 16:02:37 +0100

    #initial : initial edition of a CommController Delegate class to show possible options for handling the events raised by the MpC framework

---
+++ nasty-fish/nasty-fish/ServiceDel.swift
@@ -0,0 +1,48 @@
+//
+//  ServiceDel.swift
+//  nasty-fish
+//
+//  Created by Michael Gambitz on 11.01.17.
+//  Copyright © 2017 Gruppe 08. All rights reserved.
+//
+
+import Foundation
+
+class ServiceDel : CommControllerDelegate {
+    
+    func foundPeer() {
+        //tblPeers.reloadData()
+        //UPDATE VIEW
+    }
+    
+    
+    func lostPeer() {
+        //tblPeers.reloadData()
+        //UPDATE VIEW
+    }
+    
+    func invitationWasReceived(fromPeer: String) {
+        let alert = UIAlertController(title: "", message: "\(fromPeer) wants to chat with you.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let acceptAction: UIAlertAction = UIAlertAction(title: "Accept", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+            self.appDelegate.mpcManager.invitationHandler(true, self.appDelegate.mpcManager.session)
+        }
+        
+        let declineAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+            self.appDelegate.mpcManager.invitationHandler(false, nil)
+        }
+        
+        alert.addAction(acceptAction)
+        alert.addAction(declineAction)
+        
+        OperationQueue.main.addOperation { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func connectedWithPeer(peerID: MCPeerID){
+        OperationQueue.main.addOperation { () -> Void in
+            self.performSegue(withIdentifier: "idSegueChat", sender: self)
+        }
+    }
+}
