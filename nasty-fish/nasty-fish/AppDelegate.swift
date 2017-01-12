53495626f74bc4e0dd88cbe80a325dc2a816f28d
Micheal Gambitz <gambitz@cip.ifi.lmu.de>
2017-01-12 01:05:05 +0100

    init CommController instance

--- nasty-fish/nasty-fish/AppDelegate.swift
+++ nasty-fish/nasty-fish/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         dataController = DataController()
+        commController = CommController()
         
         // * * * DEBUG: Populate persistent storage with dummy content * * *
         let populator = Populator(dc:dataController!)
