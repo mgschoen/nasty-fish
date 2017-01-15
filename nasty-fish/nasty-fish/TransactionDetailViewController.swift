ca8e21ed858e377518af197e16e0abb3dd223ada
Qendresa Iljazi <iljazi@cip.ifi.lmu.de>
2017-01-15 20:52:40 +0100

    working on DetailView and DetailPeer

--- nasty-fish/nasty-fish/TransactionDetailViewController.swift
+++ nasty-fish/nasty-fish/TransactionDetailViewController.swift
@@ -2,44 +2,55 @@
 //  TransactionDetailViewController.swift
 //  nasty-fish
 //
-//  Created by Qendresa Iljazi on 19.12.16.
-//  Copyright © 2016 Gruppe 08. All rights reserved.
+//  Created by Qendresa Iljazi on 15.01.17.
+//  Copyright © 2017 Gruppe 08. All rights reserved.
 //
+
 
 import UIKit
 import CoreData
 
-class TransactionDetailViewController: UIViewController {
+class TransactionDetailViewController: UITableViewController {
     
-    
-    
-    @IBOutlet weak var titelDescription: UINavigationItem!
-    
+
     @IBOutlet weak var transactionDescription: UILabel!
-    @IBOutlet weak var amount: UILabel!
+    
     @IBOutlet weak var peer: UILabel!
+    @IBOutlet weak var amount: UILabel!
     
-    
+     @IBOutlet weak var titelDescription: UINavigationItem!
+    /*
+     
+     @IBOutlet weak var titelDescription: UINavigationItem!
+     
+     @IBOutlet weak var transactionDescription: UILabel!
+     @IBOutlet weak var amount: UILabel!
+     @IBOutlet weak var peer: UILabel!
+     
+     */
     
     //var text:String?
-
-
+    
+    
     // MARK: - Variables
+    //bei "Back" muss nochmal geprüft werden
     var transaction:Transaction? = nil
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = transaction?.itemDescription
-        //self.navigationController?.navigationBar.topItem?.title = "itemDescription"
+        self.navigationController?.navigationBar.topItem?.title = "Back"
         
     }
+    
     override func viewWillAppear(_ animated: Bool)
     {
-    
+        
         super.viewWillAppear(animated)
         
         print("* * * * * * Transaction description: \(transaction?.itemDescription)")
+        
         
         transactionDescription.text = transaction?.itemDescription
         
@@ -49,11 +60,12 @@ class TransactionDetailViewController: UIViewController {
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
-
+    
+    
 }
+
