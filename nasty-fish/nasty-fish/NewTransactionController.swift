158ce82445748cca6d49efbc9b8b628c9002d551
Micheal Gambitz <gambitz@cip.ifi.lmu.de>
2017-01-17 15:06:16 +0100

    #updateSemantics : Picker is now using the discoveredPartnersAsString array

--- nasty-fish/nasty-fish/NewTransactionController.swift
+++ nasty-fish/nasty-fish/NewTransactionController.swift
@@ -127,12 +127,15 @@ class NewTransactionController: UITableViewController, UIPickerViewDelegate, UIP
     
     // The number of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+        //return pickerData.count
+        return discoveredPartnersAsString.count
     }
     
     // The data to return for the row and component (column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row].customName
+
+        //return pickerData[row].customName
+        return discoveredPartnersAsString[row]
     }
     
     func pickerUpdateNames(){
