17ced5dbc11a94da8d39e1e2d65ceae7c2ec4b50
Manuel Hartmann <manuel.hartmann@mailbox.org>
2017-01-18 17:00:39 +0100

    add TransactionManager.swift

---
+++ nasty-fish/nasty-fish/TransactionManager.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionManager.swift
+//  nasty-fish
+//
+//  Created by manu on 18.01.17.
+//  Copyright © 2017 Gruppe 08. All rights reserved.
+//
+
+import Foundation
+
+class TransactionManager : NSObject {
+
+}
